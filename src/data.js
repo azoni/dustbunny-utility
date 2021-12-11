c930fe8593695fbb1f3c448e86a2b74858bd7b76
tryingmybesttoday <tryingmybestatcrypto@protonmail.com>
2021-12-10 23:52:24 -0800

    updates

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const data = {
+	COLLECTION_TRAIT: {
+		'cool-cats-nft': {
+			'tier': {
+				'cool_1': .875,
+				'cool_2': .9,
+				'wild_1': .95,
+				'wild_2': 1,
+				'classy_1': 1.1,
+				'classy_2': 1.15,
+				'exotic_1': 1.2,
+				'exotic_2': 1.25,
+			},
+		},
+		'doodles-official': {
+			'face': {
+				'neutral note': .925,
+				'mad note': .95,
+				'skelton': .95,
+			},
+			'head': {
+				'balloon': 2,
+				'devil': 1.5,
+			},
+			'background': {
+				'space': .925,
+				'sky': .925,
+				'fire': .925,
+				'holographic': .925,
+				'iridescent': .925,
+			},
+			
+		}
+	},
+}
+export default data
+
