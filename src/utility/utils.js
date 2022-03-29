945ed79d554ff457bc418111e1b962955f5abc57
tryingmybesttoday <tryingmybestatcrypto@protonmail.com>
2022-03-29 13:37:26 -0700

    linted utils

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -1,20 +1,13 @@
-async function sleep(ms){
-    // console.log('Sleeping ' + ms/1000 + ' seconds')
-    await new Promise(resolve => setTimeout(resolve, ms))
+async function sleep(ms) {
+  await new Promise((resolve) => { setTimeout(resolve, ms); });
 }
-async function start_timer(){
-    return Math.floor(+new Date())
+
+function get_ISOString(seconds) {
+  const search_time = Math.floor(+new Date()) - seconds;
+  return new Date(search_time).toISOString();
 }
-async function end_timer(start_time){
-    var end_time = Math.floor(+new Date())
-    return end_time - start_time
-}
-function get_ISOString(seconds){
-    let search_time = Math.floor(+new Date()) - seconds
-    return new Date(search_time).toISOString();
-}
-function get_ISOString_now(){
-    let search_time = Math.floor(+new Date())
-    return new Date(search_time).toISOString();
+function get_ISOString_now() {
+  const search_time = Math.floor(+new Date());
+  return new Date(search_time).toISOString();
 }
 module.exports = { sleep, get_ISOString, get_ISOString_now };
