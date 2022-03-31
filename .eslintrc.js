7d266e9934a55902ad450a578efa3163d2268cd7
Charlton <charltonuw@gmail.com>
2022-03-31 11:12:04 -0700

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,13 @@ module.exports = {
   rules: {
     camelcase: 'off',
     semi: 'off',
+    'import/extensions': 'off',
+    'no-await-in-loop': 'off',
+    'no-console': 'off',
+    'no-restricted-syntax': 'off',
+    'guard-for-in': 'off',
+    'linebreak-style': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
   },
 };
